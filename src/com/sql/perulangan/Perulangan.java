f2681a916469a03fb1fbe318075ecfba18f1d62e
eilen <eilen@DESKTOP-4P6N7M3>
2021-11-24 10:25:13 +0700

    Pertama

---
+++ src/com/sql/perulangan/Perulangan.java
@@ -0,0 +1,13 @@
+package com.sql.perulangan;
+
+public class Perulangan {
+	
+	public static void main(String[] args) {
+		//aplikasi raraban
+		int n = 8;
+		for(int i=1;i<=10;i++) {
+			System.out.printf("%d x %d = %d%n", n, i, n*i);
+		}
+	}
+
+}
